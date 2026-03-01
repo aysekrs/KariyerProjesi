c74292756c7d3444eca66cae78ac59a7556902af
aysekrs <aysekiras@icloud.com>
2026-03-01 15:52:19 +0300

    Create CareerCounselingApplication.java

---
+++ backend/CareerCounselingApplication.java
@@ -0,0 +1,12 @@
+package com.example.careercounseling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerCounselingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerCounselingApplication.class, args);
+    }
+}
