60a84ddd4db2d7d258d39bb6ab9117aea912e2f2
aysekrs <aysekiras@icloud.com>
2026-03-01 15:49:54 +0300

    Create UserRepository.java

---
+++ backend/src/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.example.careercounseling.repository;
+
+import com.example.careercounseling.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+}
+
+package com.example.careercounseling.repository;
+
+import com.example.careercounseling.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByEmail(String email);
+}
