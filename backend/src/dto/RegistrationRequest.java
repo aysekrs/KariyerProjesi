c83b621ff60d2c01f2ddd8b2e3c35782a15cde73
aysekrs <aysekiras@icloud.com>
2026-03-01 15:46:47 +0300

    Create RegistrationRequest.java

---
+++ backend/src/dto/RegistrationRequest.java
@@ -0,0 +1,46 @@
+package com.example.careercounseling.dto;
+
+import com.example.careercounseling.entity.UserRole;
+
+public class RegistrationRequest {
+
+    private String fullName;
+    private String email;
+    private String password;
+    private UserRole role;
+
+    public RegistrationRequest() {
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public void setRole(UserRole role) {
+        this.role = role;
+    }
+}
